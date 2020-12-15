eecd76338c837ed6cc1795680f2f8b386059efee
FSRR <sebastianretiz@gmail.com>
2020-12-14 20:37:02 -0500

    Todavía trabajando en la localización

--- app.py
+++ app.py
@@ -2,19 +2,20 @@ from flask import Flask
 from flask import render_template
 from flask import redirect
 from flask import url_for
-import requests
-import json
+from flask import request
 from entity import *
 from model import *
 from controller import *
+import requests as req
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    url = 'https://www.metaweather.com/api/location/418440/'
-    # url = 'https://www.metaweather.com/api/location/44418/'
-    response = requests.get(url)
+    # url = 'https://www.metaweather.com/api/location/418440/'
+    url = 'https://www.metaweather.com/api/location/44418/'
+    response = req.get(url)
 
     if response.status_code == 200:
         responseJson = response.json()
@@ -25,34 +26,37 @@ def index():
 
         data = lugar.days(responseJson)
 
+        return render_template('index.html', today = today, anothersDays = data)
+
+
+@app.route('/place/<woeid>', methods = ['GET'])
+def place(woeid):
+    if request.method == 'GET':
+        url = f'https://www.metaweather.com/api/location/{woeid}/'
+        response = req.get(url)
+
+        if response.status_code == 200:
+            responseJson = response.json()
+            lugar = DayController()
+
+            today = lugar.today(responseJson)
+
+            data = lugar.days(responseJson)
 
     return render_template('index.html', today = today, anothersDays = data)
 
 
-@app.route('/hola')
-def place(woeid):
-    url = f'https://www.metaweather.com/api/location/{woeid}/'
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        responseJson = response.json()
-        print(responseJson)
-
-    return redirect(url_for('index'))
-
-
-@app.route('/<loc>')
+@app.route('/location/<loc>', methods = ['GET'])
 def location(loc):
-    url = f'https://www.metaweather.com/api/location/search/?lattlong={loc}'
-    response = requests.get(url)
+    if request.method == 'GET':
+        url = f'https://www.metaweather.com/api/location/search/?lattlong={loc}'
+        response = req.get(url)
 
-    if response.status_code == 200:
-        responseJson = response.json()
-        woeid = responseJson[0]['woeid']
-        print(woeid)
+        if response.status_code == 200:
+            responseJson = response.json()
+            location = responseJson[0]
 
-    return redirect(url_for(f'place({woeid})'))
-
+    return redirect(f"/place/{location['woeid']}")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/location.js
+++ static/js/location.js
@@ -2,8 +2,10 @@ const ubicacion = document.getElementById('location')
 let loc
 
 function getLocation() {
-    if(navigator.geolocation) {
+    if (navigator.geolocation) {
+        console.log(navigator);
         navigator.geolocation.getCurrentPosition(showPosition)
+        console.log(navigator);
     } else {
         alert('No es compatible con tu navegador')
     }
@@ -12,18 +14,19 @@ function getLocation() {
 
 function showPosition(position) {
     loc = `${position.coords.latitude},${position.coords.longitude}`
+    ubicacion.href = `/location/${loc}`
+    console.log(loc);
     return loc
 }
 
-ubicacion.addEventListener('click', () => {
-    getLocation()
+// ubicacion.addEventListener('click', () => {
+//     console.log(ubicacion.href);
+//     if (ubicacion.href === '') alert('Tu ubicación está bloqueado, para acceder a tu ubicación haz click en el icono de ubicación de la barra de direcciones del navegador y actualiza la página')
+//     // getLocation()
+//     console.log(loc);
+//     // if(loc) {
+//     //     ubicacion.href = `/location/${loc}`
+//     // } else console.log('Click otra vez');
+// })
 
-    let xhr
-    if(XMLHttpRequest) xhr = new XMLHttpRequest()
-    else xhr = new ActiveXObject()
-
-    xhr.open('GET', `/${loc}`)
-
-    xhr.send()
-})
-
+window.onload = getLocation()
