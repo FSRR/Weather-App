d380517d1651fb7942c8664f94396b01f0f5e618
FSRR <sebastianretiz@gmail.com>
2021-01-07 11:53:50 -0500

    Version 3.2

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import render_template
 from flask import redirect
 from flask import url_for
 from flask import request
-# from entity import *
-# from model import *
-# from controller import *
+from entity import *
+from model import *
+from controller import *
 from request import *
 
 app = Flask(__name__)
@@ -13,25 +13,22 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/place/<woeid>')
 def index(woeid = 44418):
-    # lugar = DayController(woeid)
+    lugar = DayController(woeid)
 
-    # today = lugar.today()
+    today = lugar.today()
     
-    # data = lugar.days()
-
-    today = ['Rain',15,23,56,33,22, 11]
-    data = [(12,33,43), (10,33,43), (12,53,83), (21,33,42)]
+    data = lugar.days()
 
     return render_template('index.html', today = today, anothersDays = data)
 
 
-# @app.route('/location/<loc>', methods = ['GET'])
-# def location(loc):
-#     if request.method == 'GET':
-#         location = ApiRequest()
-#         woeid = location.myLocation(loc)
+@app.route('/location/<loc>', methods = ['GET'])
+def location(loc):
+    if request.method == 'GET':
+        location = ApiRequest()
+        woeid = location.myLocation(loc)
 
-#     return redirect(f"/place/{woeid}")
+    return redirect(f"/place/{woeid}")
 
 
 if __name__ == '__main__':
